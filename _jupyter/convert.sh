128b07af753362bd2aa4ca28c4504ba52313f584
“jaketae” <jaesungtae@gmail.com>
2019-11-28 08:28:26 +0900

    Reset Test

--- _jupyter/convert.sh
+++ _jupyter/convert.sh
@@ -2,8 +2,7 @@ nb=$1
 
 function convert(){
 	jupyter nbconvert $nb --to markdown
-	sed -i '1s;^;---\ntitle: TITLE\nlast_modified_at: 2019-12-01 9:50:00\ncategories:\ntags:\n---\n
-;' $${nb%.ipynb}.md
+	sed -i'' -e '1s;^;$---\ntitle: TITLE\nlast_modified_at: 2019-12-01 9:50:00\ncategories:\ntags:\n---\n;' ${nb%.ipynb}.md
 	mv ${nb%.ipynb}.md ../_posts/
 	mv ${nb%.ipynb}_files ../assets/images/
 	echo "Conversion complete!"
