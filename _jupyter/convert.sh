4ba7d9a9002a800750ed0c56467010709de98460
“jaketae” <jaesungtae@gmail.com>
2019-11-28 07:45:51 +0900

    Integrate Jupyter Notebook

---
+++ _jupyter/convert.sh
@@ -0,0 +1,12 @@
+nb=$1
+
+function convert(){
+	jupyter nbconvert $nb --to markdown
+	sed -i '1s;^;---\ntitle: TITLE\nlast_modified_at: 2019-12-01 9:50:00\ncategories:\ntags:\n---\n
+;' $${nb%.ipynb}.md
+	mv ${nb%.ipynb}.md ../_posts/
+	mv ${nb%.ipynb}_files ../assets/images/
+	echo "Conversion complete!"
+}
+
+convert
