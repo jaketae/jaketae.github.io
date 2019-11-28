a0005f1dd1278cd31ac92d800a089e4f75fda340
“jaketae” <jaesungtae@gmail.com>
2019-11-28 10:13:02 +0900

    Remove template

--- _jupyter/convert.sh
+++ _jupyter/convert.sh
@@ -3,7 +3,7 @@
 nb=$1
 
 function convert(){
-	jupyter nbconvert --to markdown --template ./custom.tpl $nb
+	jupyter nbconvert --to markdown $nb
 	mv ${nb%.ipynb}.md ../_posts/
 	mv ${nb%.ipynb}_files ../assets/images/
 	echo "Conversion complete!"
