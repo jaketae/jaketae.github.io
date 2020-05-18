83ae6d1edbb61bdd5ddbec682b6aeeaf0e85ace5
Jake Tae <jaesungtae@gmail.com>
2020-05-18 14:53:09 +0900

    feature: revamp shell script, add r tutorial 4

--- _jupyter/format.py
+++ _jupyter/format.py
@@ -1,17 +1,38 @@
 #!/usr/bin/python
 
-import sys
 import re
+import sys
 
 
-def format():
-    path = f"/Users/jaketae/Documents/GitHub/jaketae.github.io/_jupyter/{sys.argv[1]}"
-    with open(path, 'r') as file:
+def rmd(nb):
+    path = f"/Users/jaketae/Documents/GitHub/jaketae.github.io/_jupyter/{nb}"
+    with open(path, "r") as file:
         filedata = file.read()
-    filedata = re.sub("src=\"", "src=\"/assets/images/", filedata)
-    with open(path, 'w') as file:
+    filedata = re.sub('src="', 'src="/assets/images/', filedata)
+    with open(path, "w") as file:
         file.write(filedata)
 
 
-if __name__ == '__main__':
-    format()
+def ipynb(nb):
+    path = f"/Users/jaketae/Documents/GitHub/jaketae.github.io/_jupyter/{nb}"
+    with open(path, "r") as file:
+        filedata = file.read()
+    yaml = "---\ntitle: TITLE\nmathjax: true\ntoc: true\ncategories:\n  - category\ntags:\n  - tag\n---\n\n"
+    filedata = re.sub(r"!\[svg\]\(", '<img src="/assets/images/', filedata)
+    filedata = re.sub(".svg\)", '.svg">', filedata)
+    filedata = re.sub(r"!\[png\]\(", '<img src="/assets/images/', filedata)
+    filedata = re.sub(".png\)", '.png">', filedata)
+    filedata = yaml + filedata
+    with open(path, "w") as file:
+        file.write(filedata)
+
+
+if __name__ == "__main__":
+    opt = sys.argv[1]
+    nb = sys.argv[2]
+    if opt == "-r":
+        rmd(nb)
+    elif opt == "-p":
+        ipynb(nb)
+    else:
+        print("Please enter a valid option, one of -r or -p.")
--- _jupyter/format.sh
+++ _jupyter/format.sh
@@ -1,12 +1,44 @@
 #!/bin/sh
 
-nb=$1
+
+opt=$1 # -r for .Rmd or -p for .ipynb
+nb=$2
+
+
+function ipynb(){
+	jupyter nbconvert --to markdown $nb
+	python format.py $opt ${nb%.ipynb}.md
+	mv ${nb%.ipynb}.md ../_posts/
+    if [[ -d ${nb%.ipynb}_files ]]
+    then
+        echo "==========Moving image files=========="
+        mv ${nb%.ipynb}_files ../assets/images/
+    fi
+}
+
+
+function rmd(){
+    python format.py $opt $nb
+    mv $nb ../_posts/
+    if [[ -d ${nb%.Rmd}_files ]]
+    then
+        echo "==========Moving image files=========="
+        mv ${nb%.Rmd}_files ../assets/images/
+    fi
+}
+
 
 function format(){
-    mv ${nb%.md}_files ../assets/images/
-    python format.py $nb
-    mv $nb ../_posts/
+    if [[ $nb == *.ipynb ]]
+    then
+        echo "==========Starting .ipynb conversion=========="
+        ipynb
+    else
+        echo "==========Starting .Rmd formatting=========="
+        rmd
+    fi
     echo "==========Formatting complete!=========="
 }
+
 
 format
